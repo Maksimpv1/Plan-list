e6bb1019f7ecbb74482385e1ab856a034f0bc625
Maksim noyt <maksimpv1@bk.ru>
2024-01-25 10:54:04 +0300

    init:form styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import styles from './app.module.scss'
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { LoginForm } from './components/login/login';
 
+import './index.scss'
+import { RegistrationForm } from './components/registration/registration';
+
 function App() {
   return (
-    <div className={ styles.container }>
+    <div>
       <Router>
         <Routes>
           <Route path="/" element={ <LoginForm/>} />
           <Route path="/shcedule" />
+          <Route path="/registration" element={ <RegistrationForm/>} />
         </Routes>
       </Router>     
     </div>
